440f0e5751c6695e38dba80487b12e542a3a792f
olivia <obtmc@cin.ufpe.br>
2024-03-06 22:15:42 -0300

    restauração do código para o ponto em que getAllPromocoes, getPromocaoById e get da página de cadastro pegam

--- backend/src/controllers/promocao.controler.ts
+++ backend/src/controllers/promocao.controler.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from 'express';
 import PromocaoService from '../services/promocao.service'; // Importe o serviço de promoção
 import { SuccessResult } from '../utils/result'; // Importe o objeto de resultado de sucesso, se necessário
 import PromocaoEntity from '../entities/promocao.entity';
+import { get } from 'http';
 
 class PromocaoController {
   private prefix: string = '/promocoes';
@@ -15,102 +16,243 @@ class PromocaoController {
   }
 
   private initRoutes() {
-    // Rota para criar um novo promoção
+    // Rota para criar uma nova promoção
     this.router.post(`${this.prefix}/cadastro`, (req: Request, res: Response) =>
         this.createPromocao(req, res)
     );
- }
 
- /*   // Rota para obter todos os promoçãos
-    this.router.get(`${this.prefix}`, (req: Request, res: Response) =>
-        this.getAllPromocoess(req, res)
+    //Cria um GET para pagina de Cadastro 
+    this.router.get(`${this.prefix}/cadastro`, (req: Request, res: Response) =>
+      res.status(200).send({
+        text: 'Cadastro de Promoção',
+        msg: 'Cadastro de Promoção'
+      })
     );
 
-    // Rota para obter um promoção pelo ID
+    // Rota para buscar uma promoção pelo ID
     this.router.get(`${this.prefix}/:id`, (req: Request, res: Response) =>
         this.getPromocaoById(req, res)
     );
 
-    // Rota para atualizar um promoção pelo ID
+    // Rota para atualizar uma promoção
     this.router.put(`${this.prefix}/:id`, (req: Request, res: Response) =>
-      this.updatePromocaoById(req, res)
+        this.updatePromocao(req, res)
     );
-  }*/
 
-  private async createPromocao(req: Request, res: Response) {
+    // Rota para pegar todas às promoções
+    this.router.get(`${this.prefix}`, (req: Request, res: Response) =>
+        this.getAllPromocoes(req, res)
+    );
+ }
+
+ private async createPromocao(req: Request, res: Response) {
+  // Extrai os dados do corpo da requisição
+  const promocao = await this.promocaoService.createPromocao(new PromocaoEntity(req.body));
+
+  // Retorna o novo promoção criado
+  return new SuccessResult({
+      msg: 'Cadastro de promoção concluído com sucesso!',
+      data: promocao,
+  }).handle(res);
+}
+
+  private async createPromocao2(req: Request, res: Response) {
     // Extrai os dados do corpo da requisição
-    const promocao = await this.promocaoService.createPromocao(new PromocaoEntity(req.body));
+    const promocao = await this.promocaoService.createPromocao2(new PromocaoEntity(req.body));
 
-    // Retorna o novo promoção criado
+    // Retorna a nova promoção criada
     return new SuccessResult({
-        msg: 'Cadastro de promoção concluído com sucesso!',
+        msg: promocao.msg,
+        data: promocao.data,
+        code: promocao.code
+    }).handle(res);
+  }
+
+  private async updatePromocao(req: Request, res: Response) {
+    // Extrai o ID do usuário da URL
+    const promocaoId = req.params.id;
+    
+    // Extrai os dados do corpo da requisição
+    const promocao = await this.promocaoService.updatePromocaoById(promocaoId, new PromocaoEntity(req.body));
+    
+    if (!promocao) {
+      // Retorna um erro caso a promoção não seja encontrada
+      return new SuccessResult({
+        msg: 'Promoção não encontrada',
+        data: null,
+        msgCode: 'promocao_not_found',
+        code: 404
+      }).handle(res);
+    }
+    // Retorna a promoção atualizada
+    return new SuccessResult({
+        msg: 'As Informações foram atualizadas com sucesso',
+        data: promocao,
+    }).handle(res);
+  }
+
+  private async getPromocaoById(req: Request, res: Response) {
+    // Extrai o ID da requisição
+    const id = req.params.id;
+
+    // Busca a promoção pelo ID
+    const promocao = await this.promocaoService.getPromocaoById(id);
+
+    if (!promocao) {
+      // Retorna um erro caso a promoção não seja encontrada
+      return new SuccessResult({
+          msg: 'Promoção não encontrada',
+          data: null,
+          msgCode: 'promocao_not_found',
+          code: 404
+      }).handle(res);
+    }
+
+    // Retorna a promoção encontrada
+    return new SuccessResult({
+        msg: 'Promoção encontrada',
         data: promocao,
     }).handle(res);
   }
 
   private async getAllPromocoes(req: Request, res: Response) {
-    try {
-      // Chama o serviço para obter todos os promoçãos
-      const promocoes = await this.promocaoService.getAllPromocoes();
+    // Busca todas as promoções
+    const promocoes = await this.promocaoService.getAllPromocoes();
 
-      // Retorna a lista de promoçãos
-      return new SuccessResult({
-        msg: 'Lista de promoçãos',
+    if (!promocoes) {
+        // Retorna um erro caso não haja promoções
+        return new SuccessResult({
+            msg: 'Nenhuma promoção encontrada',
+            data: null,
+            msgCode: 'promocoes_not_found',
+            code: 404
+        }).handle(res);
+    } 
+    // Retorna todas as promoções
+    return new SuccessResult({
+        msg: 'Lista de todas as promoções',
         data: promocoes,
-      }).handle(res);
-    } catch (error) {
-      // Trata erros de forma adequada
-      console.error('Erro ao obter promoçãos:', error);
-      return res.status(500).json({ error: 'Erro ao obter promoçãos' });
-    }
-  }
-
-  private async getPromocaoById(req: Request, res: Response) {
-    try {
-      // Extrai o ID do parâmetro da requisição
-      const promocaoId = req.params.id;
-
-      // Chama o serviço para obter o promoção pelo ID
-      const promocao = await this.promocaoService.getPromocaoById(promocaoId);
-
-      // Verifica se o promoção foi encontrado
-      if (!promocao) {
-        return res.status(404).json({ error: 'promoção não encontrado' });
-      }
-
-      // Retorna o promoção encontrado
-      return new SuccessResult({
-        msg: 'promoção encontrado',
-        data: promocao,
-      }).handle(res);
-    } catch (error) {
-      // Trata erros de forma adequada
-      console.error('Erro ao obter promoção pelo ID:', error);
-      return res.status(500).json({ error: 'Erro ao obter promoção pelo ID' });
-    }
-  }
-
-  private async updatepromocaoById(req: Request, res: Response) {
-    try {
-      // Extrai o ID do parâmetro da requisição
-      const promocaoId = req.params.id;
-      // Extrai os dados atualizados do corpo da requisição
-      const updatedData = req.body;
-
-      // Chama o serviço para atualizar o promoção pelo ID
-      const updatedPromocao = await this.promocaoService.updatePromocaoById(promocaoId, updatedData);
-
-      // Retorna o promoção atualizado
-      return new SuccessResult({
-        msg: 'promoção atualizado com sucesso',
-        data: updatedPromocao,
-      }).handle(res);
-    } catch (error) {
-      // Trata erros de forma adequada
-      console.error('Erro ao atualizar promoção pelo ID:', error);
-      return res.status(500).json({ error: 'Erro ao atualizar promoção pelo ID' });
-    }
+    }).handle(res);
   }
 }
 
 export default PromocaoController;
+
+
+
+// import { Router, Request, Response } from 'express';
+// import PromocaoService from '../services/promocao.service'; // Importe o serviço de promoção
+// import { SuccessResult } from '../utils/result'; // Importe o objeto de resultado de sucesso, se necessário
+// import PromocaoEntity from '../entities/promocao.entity';
+
+// class PromocaoController {
+//   private prefix: string = '/promocoes';
+//   public router: Router;
+//   private promocaoService: PromocaoService;
+
+//   constructor(router: Router, promocaoService: PromocaoService) {
+//     this.router = router;
+//     this.promocaoService = promocaoService;
+//     this.initRoutes();
+//   }
+
+//   private initRoutes() {
+//     // Rota para criar um novo promoção
+//     this.router.post(`${this.prefix}/cadastro`, (req: Request, res: Response) =>
+//         this.createPromocao(req, res)
+//     );
+//  }
+
+//  /*   // Rota para obter todos os promoçãos
+//     this.router.get(`${this.prefix}`, (req: Request, res: Response) =>
+//         this.getAllPromocoess(req, res)
+//     );
+
+//     // Rota para obter um promoção pelo ID
+//     this.router.get(`${this.prefix}/:id`, (req: Request, res: Response) =>
+//         this.getPromocaoById(req, res)
+//     );
+
+//     // Rota para atualizar um promoção pelo ID
+//     this.router.put(`${this.prefix}/:id`, (req: Request, res: Response) =>
+//       this.updatePromocaoById(req, res)
+//     );
+//   }*/
+
+//   private async createPromocao(req: Request, res: Response) {
+//     // Extrai os dados do corpo da requisição
+//     const promocao = await this.promocaoService.createPromocao(new PromocaoEntity(req.body));
+
+//     // Retorna o novo promoção criado
+//     return new SuccessResult({
+//         msg: 'Cadastro de promoção concluído com sucesso!',
+//         data: promocao,
+//     }).handle(res);
+//   }
+
+//   private async getAllPromocoes(req: Request, res: Response) {
+//     try {
+//       // Chama o serviço para obter todos os promoçãos
+//       const promocoes = await this.promocaoService.getAllPromocoes();
+
+//       // Retorna a lista de promoçãos
+//       return new SuccessResult({
+//         msg: 'Lista de promoçãos',
+//         data: promocoes,
+//       }).handle(res);
+//     } catch (error) {
+//       // Trata erros de forma adequada
+//       console.error('Erro ao obter promoçãos:', error);
+//       return res.status(500).json({ error: 'Erro ao obter promoçãos' });
+//     }
+//   }
+
+//   private async getPromocaoById(req: Request, res: Response) {
+//     try {
+//       // Extrai o ID do parâmetro da requisição
+//       const promocaoId = req.params.id;
+
+//       // Chama o serviço para obter o promoção pelo ID
+//       const promocao = await this.promocaoService.getPromocaoById(promocaoId);
+
+//       // Verifica se o promoção foi encontrado
+//       if (!promocao) {
+//         return res.status(404).json({ error: 'promoção não encontrado' });
+//       }
+
+//       // Retorna o promoção encontrado
+//       return new SuccessResult({
+//         msg: 'promoção encontrado',
+//         data: promocao,
+//       }).handle(res);
+//     } catch (error) {
+//       // Trata erros de forma adequada
+//       console.error('Erro ao obter promoção pelo ID:', error);
+//       return res.status(500).json({ error: 'Erro ao obter promoção pelo ID' });
+//     }
+//   }
+
+//   private async updatepromocaoById(req: Request, res: Response) {
+//     try {
+//       // Extrai o ID do parâmetro da requisição
+//       const promocaoId = req.params.id;
+//       // Extrai os dados atualizados do corpo da requisição
+//       const updatedData = req.body;
+
+//       // Chama o serviço para atualizar o promoção pelo ID
+//       const updatedPromocao = await this.promocaoService.updatePromocaoById(promocaoId, updatedData);
+
+//       // Retorna o promoção atualizado
+//       return new SuccessResult({
+//         msg: 'promoção atualizado com sucesso',
+//         data: updatedPromocao,
+//       }).handle(res);
+//     } catch (error) {
+//       // Trata erros de forma adequada
+//       console.error('Erro ao atualizar promoção pelo ID:', error);
+//       return res.status(500).json({ error: 'Erro ao atualizar promoção pelo ID' });
+//     }
+//   }
+// }
+
+// export default PromocaoController;
--- backend/src/services/promocao.service.ts
+++ backend/src/services/promocao.service.ts
@@ -2,7 +2,7 @@ import PromocaoEntity from '../entities/promocao.entity'; // Importa a entidade 
 import PromocaoModel from '../models/promocao.model'; // Importa o modelo de promoção
 import PromocaoRepository from '../repositories/promocao.repository'; // Importa o repositório de promoção
 import { HttpNotFoundError } from '../utils/errors/http.error'; // Importa o erro de não encontrado HTTP
-
+import { SuccessResult } from '../utils/result'; // Importe o objeto de resultado de sucesso, se necessário
 class PromocaoServiceMessageCode {
     public static readonly promocao_not_found = 'promocao_not_found';
 }
@@ -44,6 +44,20 @@ public async getAllPromocoes(): Promise<PromocaoModel[]> {
     return promocaoModel;
   }
 
+  public async createPromocao2(data: PromocaoEntity): Promise<SuccessResult> {
+    let promocao = new PromocaoModel(data);
+    if (this.validaPromocao(promocao).status == 200){
+      const promocaoEntity = await this.promocaoRepository.createPromocao(data);
+      promocao = new PromocaoModel(promocaoEntity);
+    }
+
+    return new SuccessResult({
+      msg: this.validaPromocao(promocao).msg,
+      data: promocao,
+      code: this.validaPromocao(promocao).status
+    });
+  }
+
   public async updatePromocaoById(id: string, data: PromocaoEntity): Promise<PromocaoModel> {
     const promocaoEntity = await this.promocaoRepository.updatePromocaoById(id, data);
 
@@ -59,7 +73,48 @@ public async getAllPromocoes(): Promise<PromocaoModel[]> {
     return promocaoModel;
   }
 
+  private validaPromocao (promocaoData: PromocaoModel){
+    //const promocaoData = new PromocaoModel(promocao);
+    let returnData = {
+      status: 400,
+      msg: ""
+    };
+    const verifBranco = promocaoData.verificarBranco(promocaoData);
+    if(promocaoData.verificarExistente(promocaoData.id)) {
+        console.log("Id: " + promocaoData.id);
+        console.log("Nome: " + promocaoData.nome);
+        returnData.status = 400;
+        returnData.msg = "Promoção com ID existente";
+        console.log('1.1 '+ JSON.stringify(returnData) + "Id: " + promocaoData.id);
+  
+    } else if(verifBranco == 1) {
+        returnData.status = 400;
+        returnData.msg = "Campos em branco";
+        console.log('1.2 '+ JSON.stringify(returnData));
+  
+    } else if(verifBranco == 2) {
+        returnData.status = 200;
+        returnData.msg ="Cadastro de promoção concluído com sucesso!"
+        promocaoData.valor = '10';
+        //response = await request.post('/api/promocoes/cadastro').send(JSON.stringify(promocaoData));
+        //console.log('1 '+ JSON.stringify(response));
+  
+    } else if(!promocaoData.verificarValor()) {
+        returnData.status = 400;
+        returnData.msg = "Valor inválido";
+        console.log('1 '+ JSON.stringify(returnData));
+        
+    } else {
+        returnData.status = 200;
+        returnData.msg ="Cadastro de promoção concluído com sucesso!"  ;
+        //response = await request.post('/api/promocoes/cadastro').send(JSON.stringify(promocaoData));
+        console.log('2 '+ JSON.stringify(returnData));
+    }
 
+    return returnData;
+}
+
+  
 }
 
 export default PromocaoService;
