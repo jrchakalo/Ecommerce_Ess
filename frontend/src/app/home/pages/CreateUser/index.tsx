9532cf9df9ad06e2192a35eab86e9a1d1f4425b5
jrchakalo <jisj@cin.ufpe.br>
2024-03-11 01:31:41 -0300

    refactor: improving frontend

--- frontend/src/app/home/pages/CreateUser/index.tsx
+++ frontend/src/app/home/pages/CreateUser/index.tsx
@@ -5,8 +5,9 @@ import { useContext, useEffect } from "react";
 import { UserContext } from "../../context/UserContext";
 import { UserFormSchema, UserFormType } from "../../forms/UserForm";
 import { Link, Navigate } from "react-router-dom";
-import Button from "../../../../shared/components/Button";
 import InputMask from "react-input-mask";
+import 'semantic-ui-css/semantic.min.css';
+import { Form, Grid, Header, Button, Segment, Divider } from "semantic-ui-react";  
 
 const CreateUser = () => {
   const { state, prevState, service } = useContext(UserContext);
@@ -38,93 +39,120 @@ const CreateUser = () => {
 
   return (
     <section className={styles.container}>
-      <h1 className={styles.title}>Cadastro de Usuário</h1>
-      <form className={styles.formContainer} onSubmit={handleSubmit(onSubmit)}>
-        <div className={styles.formInputContainer}>
-          <input
-            data-cy="input-name"
-            {...register("nome")}
-            placeholder="Digite seu nome"
-            className={styles.formInput}
-          />
-          {errors.nome && (
-            <span data-cy="input-name-error" className={styles.formError}>
-              {errors.nome?.message}
-            </span>
-          )}
-        </div>
-  
-        <div className={styles.formInputContainer}>
-          <input
-            data-cy="input-cpf"
-            {...register("cpf")}
-            placeholder="Digite seu CPF"
-            className={styles.formInput}
-          />
-          {errors.cpf && (
-            <span data-cy="input-cpf-error" className={styles.formError}>
-              {errors.cpf.message}
-            </span>
-          )}
-        </div>
-  
-        <div className={styles.formInputContainer}>
-          <InputMask
-            mask="99/99/9999"
-            data-cy="input-birthdate"
-            {...register("dataNascimento")}
-            placeholder="Digite sua data de nascimento"
-            className={styles.formInput}
-          />
-          {errors.dataNascimento && (
-            <span data-cy="input-birthdate-error" className={styles.formError}>
-              {errors.dataNascimento.message}
-            </span>
-          )}
-        </div>
-  
-        <div className={styles.formInputContainer}>
-          <input
-            data-cy="input-email"
-            {...register("email")}
-            placeholder="Digite seu email"
-            className={styles.formInput}
-          />
-          {errors.email && (
-            <span data-cy="input-email-error" className={styles.formError}>
-              {errors.email.message}
-            </span>
-          )}
-        </div>
-  
-        <div className={styles.formInputContainer}>
-          <input
-            data-cy="input-login"
-            {...register("login")}
-            placeholder="Digite seu login"
-            className={styles.formInput}
-          />
-        </div>
-  
-        <div className={styles.formInputContainer}>
-          <input
-            data-cy="input-password"
-            type="password"
-            {...register("senha")}
-            placeholder="Digite sua senha"
-            className={styles.formInput}
-          />
-          {errors.senha && (
-            <span data-cy="input-password-error" className={styles.formError}>
-              {errors.senha.message}
-            </span>
-          )}
-        </div>
-  
-        <Button data-cy="create" type="submit" disabled={state.createUserRequestStatus.isLoading()}>
-          {state.createUserRequestStatus.isLoading() ? "Criando..." : "CRIAR"}
-        </Button>
-      </form>
+      <Grid centered>
+        <Grid.Column style={{maxWidth:750, marginTop:250,}}>
+          <Segment>
+            <Header size='huge' bold>Cadastre-se</Header>
+            <Form className={styles.formContainer} onSubmit={handleSubmit(onSubmit)} size='huge'>
+              <Form.Field>
+                <label>Nome</label>
+                <input
+                  data-cy="input-name"
+                  {...register("nome")}
+                  placeholder="Digite seu nome"
+                />
+                {errors.nome && (
+                  <span data-cy="input-name-error" className={styles.formError}>
+                    {errors.nome?.message}
+                  </span>
+                )}
+              </Form.Field>
+                
+              <Form.Field>
+                <label>CPF</label>
+                <input
+                  data-cy="input-cpf"
+                  {...register("cpf")}
+                  placeholder="Digite seu CPF"
+                    
+                />
+                {errors.cpf && (
+                  <span data-cy="input-cpf-error" className={styles.formError}>
+                    {errors.cpf.message}
+                  </span>
+                )}
+              </Form.Field>
+              
+              <Form.Field>
+                <label>Data de Nascimento</label>
+                <InputMask
+                  mask="99/99/9999"
+                  data-cy="input-birthdate"
+                  {...register("dataNascimento")}
+                  placeholder="Digite sua data de nascimento"
+                  className={styles.formInput}
+                />
+                {errors.dataNascimento && (
+                  <span data-cy="input-birthdate-error" className={styles.formError}>
+                    {errors.dataNascimento.message}
+                  </span>
+                )}
+              </Form.Field>
+          
+              <Form.Field>
+                <label>Email</label>
+                <input
+                  data-cy="input-email"
+                  {...register("email")}
+                  placeholder="Digite seu email"
+                  className={styles.formInputCadastro}
+                />
+                {errors.email && (
+                  <span data-cy="input-email-error" className={styles.formError}>
+                    {errors.email.message}
+                  </span>
+                )}
+              </Form.Field>
+
+              <Form.Field>
+                <label>Login</label>
+                <input
+                  data-cy="input-login"
+                  {...register("login")}
+                  placeholder="Digite seu login"
+                  className={styles.formInput}
+                />
+              </Form.Field>
+          
+              <Form.Field>
+                <label>Senha</label>
+                <input
+                  data-cy="input-password"
+                  type="password"
+                  {...register("senha")}
+                  placeholder="Digite sua senha"
+                  className={styles.formInput}
+                />
+                {errors.senha && (
+                  <span data-cy="input-password-error" className={styles.formError}>
+                    {errors.senha.message}
+                  </span>
+                )}
+              </Form.Field>
+        
+              <Button fluid color='orange' size='medium' data-cy="create" type="submit" disabled={state.createUserRequestStatus.isLoading()}>
+                {state.createUserRequestStatus.isLoading() ? "Criando..." : "CRIAR"}
+              </Button>
+              <Divider horizontal>Já tem um Login?</Divider>
+              <Link to="/login" data-cy="login">
+              <Button
+                color='orange'
+                content='LOGIN'
+                icon='user'
+                labelPosition='right'
+                data-cy="login"
+                className="fluid"
+              />
+            </Link>
+              
+            </Form>
+          </Segment>
+          <Link data-cy="go-home" to="/home">
+            <Button>Voltar para o início</Button>
+          </Link>
+        </Grid.Column>
+      </Grid>
+      
   
       {state.createUserRequestStatus.isSuccess() && (
         <><p className={styles.successMessage}>Usuário criado com sucesso!</p><Navigate to="/login" replace /></>
@@ -134,9 +162,7 @@ const CreateUser = () => {
         <p className={styles.errorMessage}>{state.createUserRequestStatus.error.message}</p>
       )}
   
-      <Link data-cy="view-users" to="/home">
-        Voltar para início
-      </Link>
+      
     </section>
   );  
 };
--- frontend/src/app/home/pages/Login/index.tsx
+++ frontend/src/app/home/pages/Login/index.tsx
@@ -1,3 +1,4 @@
+import 'semantic-ui-css/semantic.min.css';
 import { SubmitHandler, useForm } from "react-hook-form";
 import styles from "./index.module.css";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -5,7 +6,7 @@ import { useContext, useEffect } from "react";
 import { LoginContext } from "../../context/LoginContext";
 import { LoginFormSchema, LoginFormType } from "../../forms/LoginForm";
 import { Link, Navigate } from "react-router-dom";
-import Button from "../../../../shared/components/Button";
+import { Segment, Button, Divider } from "semantic-ui-react";  
 
 const Login = () => {
   const { state, prevState, service } = useContext(LoginContext);
@@ -35,53 +36,74 @@ const Login = () => {
 
   return (
     <section className={styles.container}>
-      <h1 className={styles.title}>Login</h1>
-      <form className={styles.formContainer} onSubmit={handleSubmit(onSubmit)}>
-        <div className={styles.formInputContainer}>
-          <input
-            data-cy="input-login"
-            {...register("login")}
-            placeholder="Digite seu login"
-            className={styles.formInput}
-          />
-          {errors.login && (
-            <span data-cy="input-login-error" className={styles.formError}>
-              {errors.login?.message}
-            </span>
-          )}
+      <Segment basic padded='very' floated='left'>
+        <h1 className={styles.title}>Faça seu login</h1>
+      </Segment> 
+      <div className={styles.bottomContent}>
+        <form className={styles.formContainer} onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.formInputContainer}>
+            <input
+              data-cy="input-login"
+              {...register("login")}
+              placeholder="Digite seu login"
+              className={styles.formInput}
+            />
+            <div className={styles.bottomContent}>
+              {errors.login && (
+              <span data-cy="input-login-error" className={styles.formError}>
+                {errors.login?.message}
+              </span>
+              )}
+            </div>
+          </div>
+    
+          <div className={styles.formInputContainer}>
+            <input
+              data-cy="input-password"
+              type="password"
+              {...register("senha")}
+              placeholder="Digite sua senha"
+              className={styles.formInput}
+            />
+            {errors.senha && (
+              <span data-cy="input-password-error" className={styles.formError}>
+                {errors.senha.message}
+              </span>
+            )}
+          </div>
+    
+          <Button color="orange" data-cy="login-button" type="submit" disabled={state.loginRequestStatus.isLoading()}>
+            {state.loginRequestStatus.isLoading() ? "Logando..." : "Login"}
+          </Button>
+          
+            {state.loginRequestStatus.isFailure() && (
+            <p className={styles.errorMessage}>{state.loginRequestStatus.error.message}</p>
+            )}
+        </form>
+        <div className={styles.bottomContent}>     
+          <Divider horizontal>Não possui uma conta?</Divider>
+            <Link to="/create-user" data-cy="register">
+              <Button
+                color='orange'
+                content='Cadastre-se'
+                icon='add'
+                labelPosition='right'
+                data-cy="register"
+                className="fluid"
+              />
+            </Link>
+                
+          <div className={styles.linkButtonContainer}>
+            <Link data-cy="go-home" to="/home">
+              <Button size='tiny'>Voltar para o início</Button>
+            </Link>
+          </div>
         </div>
-  
-        <div className={styles.formInputContainer}>
-          <input
-            data-cy="input-password"
-            type="password"
-            {...register("senha")}
-            placeholder="Digite sua senha"
-            className={styles.formInput}
-          />
-          {errors.senha && (
-            <span data-cy="input-password-error" className={styles.formError}>
-              {errors.senha.message}
-            </span>
-          )}
-        </div>
-  
-        <Button data-cy="login-button" type="submit" disabled={state.loginRequestStatus.isLoading()}>
-          {state.loginRequestStatus.isLoading() ? "Logando..." : "LOGIN"}
-        </Button>
-      </form>
+      </div> 
   
       {state.loginRequestStatus.isSuccess() && (
         <Navigate to={`/home`} />
       )}
-
-      {state.loginRequestStatus.isFailure() && (
-        <p className={styles.errorMessage}>{state.loginRequestStatus.error.message}</p>
-      )}
-  
-      <Link data-cy="back-to-home" to="/home">
-        Voltar para início
-      </Link>
     </section>
   );  
 };
